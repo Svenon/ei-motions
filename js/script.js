91b8c8770ce1ea6e3c3be961ff4a10947f3ef77d
Evgen_I <116200074+Svenon@users.noreply.github.com>
2024-02-14 15:53:35 +0200

    first test
    some description

---
+++ js/script.js
@@ -0,0 +1,13 @@
+console.log(' script work');
+
+const widthHeight = document.getElementById("lottieDiv");
+var animationData = "animations/examle.json";
+var params = { container: widthHeight, path: 'animations/example2.json', renderer: 'svg', loop: true, autoplay: true, name: "Demo Animation", };
+var anim;
+
+anim = lottie.loadAnimation(params);
+
+onmousemove = function (e) {
+    console.log("mouse location:", 100/widthHeight.offsetWidth*e.clientX, "%", e.clientY);
+    lottie.goToAndStop(181 * (1 / widthHeight.offsetWidth * e.clientX), true, "Demo Animation");
+}
