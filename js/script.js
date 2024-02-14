22b8a88183c57bf2512809d15437e1eb12d4430e
Evgen_I <116200074+Svenon@users.noreply.github.com>
2024-02-14 21:18:21 +0200

    Update script.js add mouse limit for animations v2
    Виправлено

--- js/script.js
+++ js/script.js
@@ -9,9 +9,10 @@ anim = lottie.loadAnimation(params);
 
 onmousemove = function (e) {
     var mouseW = 1 / widthHeight.offsetWidth * e.clientX;
-    //console.log("mouse location:", 100/widthHeight.offsetWidth*e.clientX, "%", e.clientY);
+    
     if(mouseW > 1){
         mouseW = 1;
     }
-    lottie.goToAndStop(181 * mouseW, true, "Demo Animation");
+    console.log("mouse location:", 100*mouseW, "%");
+    lottie.goToAndStop(180 * mouseW, true, "Demo Animation");
 }
