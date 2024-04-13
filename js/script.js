5de842bb5d1051363f4b8d643c0ea17fbd61cf23
Evgen_I <116200074+Svenon@users.noreply.github.com>
2024-04-13 19:54:49 +0300

    Add lottie animations of GoldMedal

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 console.log(' script work');
 
 const widthHeight = document.getElementById("lottieDiv");
-var animationData = "animations/example.json";
-var params = { container: widthHeight, path: 'animations/example.json', renderer: 'svg', loop: true, autoplay: true, name: "Demo Animation", };
+var animationData = "animations/horizont.json";
+var params = { container: widthHeight, path: animationData, renderer: 'svg', loop: true, autoplay: true, name: "Demo Animation", };
 var anim;
 
 anim = lottie.loadAnimation(params);
@@ -13,6 +13,10 @@ onmousemove = function (e) {
     if(mouseW > 1){
         mouseW = 1;
     }
-    console.log("mouse location:", 100*mouseW, "%");
-    lottie.goToAndStop(180 * mouseW, true, "Demo Animation");
+    //console.log("mouse location:", 100*mouseW, "%");
+    //console.log("totalFrames", anim.totalFrames);
+    //lottie.goToAndStop((anim.totalFrames-1) * mouseW, true, "Demo Animation");
+}
+anim.onEnterFrame = function(e){
+    console.log("currentTime: ", e.currentTime);
 }
